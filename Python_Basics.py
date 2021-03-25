861b763263db3b129c0b0742f02c0e92b338ddf5
loanne5 <58616743+loanne5@users.noreply.github.com>
2021-03-25 14:58:18 +0800

    Create Python_Basics.py

---
+++ Python_Basics.py
@@ -0,0 +1,71 @@
+# Print the sum of 7 and 10
+print(7 + 10)
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+
+#------------------------------------------
+#Variable and types
+# Create a variable savings
+savings=100
+# Print out savings
+print(savings)
+
+#Calculations with variables
+# Create a variable savings
+savings = 100
+# Create a variable growth_multiplier
+growth_multiplier=1.1
+# Calculate result
+result = savings * growth_multiplier ** 7
+# Print out result
+print(result)
+
+#Other Variables types
+# Create a variable desc
+desc = "compound interest"
+# Create a variable profitable
+profitable = True
+
+#Operations with other types
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+# Assign product of growth_multiplier and savings to year1
+#Calculate the product of savings and growth_multiplier. Store the result in year1.
+year1 = savings * growth_multiplier
+# Print the type of year1
+#What do you think the resulting type will be? Find out by printing out the type of year1.
+print(type(year1))
+# Assign sum of desc and desc to doubledesc
+#Calculate the sum of desc and desc and store the result in a new variable doubledesc.
+doubledesc = desc + desc
+# Print out doubledesc
+print(doubledesc)
+
+#Type conversion
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+# Fix the printout
+#Fix the code such that the printout runs without errors; use the function str() to convert the variables to strings.
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+# Definition of pi_string
+pi_string = 3.1415926
+# Convert pi_string into float: pi_float
+#Convert the variable pi_string to a float and store this float as a new variable, pi_float.
+float(pi_string)
+pi_float = pi_string
+#pi_float= float(pi_string) same lang yan 
+print(pi_float)
+
+
+
